e58cac27de1473ac68454656a970af1d327ee812
Mutayyab Imran <mutayyab786yo@gmail.com>
2022-10-18 15:49:24 +0500

    Update To Login With Email

--- Connect_To_Database/login.aspx.cs
+++ Connect_To_Database/login.aspx.cs
@@ -15,9 +15,10 @@ namespace Connect_To_Database
         protected void Button1_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(cs);
-            string query = "select * from signup_tbl where username=@user and password=@pass";
+            string query = "select * from signup_tbl where username=@user and password=@pass;select * from signup_tbl where email=@email and password=@pass";
             SqlCommand cmd = new SqlCommand(query, con);
             cmd.Parameters.AddWithValue("@user",TextBox1.Text);
+            cmd.Parameters.AddWithValue("@email",TextBox1.Text);
             cmd.Parameters.AddWithValue("@pass",TextBox2.Text);
             con.Open();
             SqlDataReader dr = cmd.ExecuteReader();
@@ -40,6 +41,25 @@ namespace Connect_To_Database
 
                 }
 
+            }
+            else if (dr.NextResult()==true)
+            {
+                dr.Read();
+                var dbemail = dr[6].ToString();
+                var dbPassword = dr[7].ToString();
+                dr.Close();
+                if (dbemail == TextBox1.Text && dbPassword == TextBox2.Text)
+                {
+
+                    con.Close();
+                    Response.Redirect("~/Dashboard.aspx");
+
+                }
+                else
+                {
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script> alert('Login Faild');</script>");
+
+                }
             }
             else
             {
