b259a6e3bb241b90e4d8314e44e267a2d85975aa
Mutayyab Imran <mutayyab786yo@gmail.com>
2022-10-18 15:39:07 +0500

    Update

--- Connect_To_Database/login.aspx.cs
+++ Connect_To_Database/login.aspx.cs
@@ -15,61 +15,29 @@ namespace Connect_To_Database
         protected void Button1_Click(object sender, EventArgs e)
         {
             SqlConnection con = new SqlConnection(cs);
-            string query = "select * from signup_tbl where username=@username and password=@pass;select * from signup_tbl where email=@email and password=@pass";
-            string query2 = "select * from signup_tbl where email=@email and password=@pass";
+            string query = "select * from signup_tbl where username=@user and password=@pass";
             SqlCommand cmd = new SqlCommand(query, con);
-            SqlCommand cmd2 = new SqlCommand(query2, con);
-            cmd.Parameters.AddWithValue("@username", TextBox1.Text);
-            cmd.Parameters.AddWithValue("@pass", TextBox2.Text);
-            cmd.Parameters.AddWithValue("@email", TextBox1.Text);
-            cmd2.Parameters.AddWithValue("@email", TextBox1.Text);
-            cmd2.Parameters.AddWithValue("@pass", TextBox2.Text);
+            cmd.Parameters.AddWithValue("@user",TextBox1.Text);
+            cmd.Parameters.AddWithValue("@pass",TextBox2.Text);
             con.Open();
             SqlDataReader dr = cmd.ExecuteReader();
-
-            //con.Close();
-
-            //con.Open();
-            bool res = dr.NextResult();
-            //SqlDataReader dr2 = cmd.ExecuteReader();
-            //con.Close();
-
-
-            //con.Open();
-            if ((dr.HasRows == true) || (res == true) /*|| (dr2.HasRows == true)*/)
+            if (dr.HasRows == true)
             {
-                var dbpassword = "";
-                var dbusername = "";
-                var useremail = "";
-
-                if (dr.HasRows == true || (res == true))
+                dr.Read();
+                var dbusername = dr[5].ToString();
+                var dbPassword = dr[7].ToString();
+                dr.Close();
+                if (dbusername == TextBox1.Text && dbPassword == TextBox2.Text)
                 {
-                    dr.Read();
-                    dbusername = dr[5].ToString();
-                    dbpassword = dr[7].ToString();
-                    useremail = dr[6].ToString();
-                    dr.Close();
 
-                }
-                //else if (dr2.HasRows == true)
-                //{
-                //    dr2.Read();
-                //    dbusername = dr[5].ToString();
-                //    dbpassword = dr[7].ToString();
-                //    useremail = dr[6].ToString();
-                //    dr2.Close();
-                //}
-
-                if (((dbusername == TextBox1.Text) || useremail == TextBox1.Text) && (dbpassword == TextBox2.Text))
-                {
-                    Session["user"] = TextBox1.Text;
                     con.Close();
                     Response.Redirect("~/Dashboard.aspx");
 
                 }
                 else
                 {
-                    Page.ClientScript.RegisterStartupScript(this.GetType(), "Scripts", "<script> alert('Login Faild');</script>");
+                    Page.ClientScript.RegisterStartupScript(this.GetType(), "scripts", "<script> alert('Login Faild');</script>");
+
                 }
 
             }
@@ -80,9 +48,10 @@ namespace Connect_To_Database
             }
             con.Close();
 
-
-
-
         }
+
+
+
     }
+
 }
